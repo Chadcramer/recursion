6f8e0fed3e03fc2791b107aee2a15b62e8f8353f
Chad Cramer <chcr6493@colorado.edu>
2019-03-28 11:37:16 -0700

    stringifyJSON complete

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,52 @@
 
 // but you don't so you're going to write it from scratch:
 
+// working 
 var stringifyJSON = function(obj) {
-  // your code goes here
+    var arrOfKeyVals = [];
+    var arrVals = [];
+    var objKeys = [];
+
+  if(obj === null){
+    return '' + obj + '';
+  }
+
+  if(typeof obj === 'number' || typeof obj === 'boolean'){
+    return  obj.toString() ;
+  }
+
+  if(typeof obj === 'string'){
+    return '\"'+ obj + '\"';
+  }
+
+  if(Array.isArray(obj)){
+    if(obj[0] === undefined){
+      return '[]';
+    } else {
+      obj.forEach(function(el) {
+      arrVals.push(stringifyJSON(el));
+      });
+      return '[' + arrVals + ']';
+    }
+  }
+
+  if(typeof obj === 'object'){
+    var arrOfKeyVals = [];
+    var objKeys = Object.keys(obj);
+    var objVals = Object.values(obj);
+      console.log(objKeys, objVals);
+    for(let i = 0; i < objKeys.length; i++){
+      // check for nested obj, use recursion     
+        
+      if (typeof objVals[i] === 'string'){
+          arrOfKeyVals.push(`"${objKeys[i]}":"${objVals[i]}"`);       
+      } else if(typeof objVals[i] === 'number' || typeof objVals[i] === 'boolean'){
+        arrOfKeyVals.push(`"${objKeys[i]}":${objVals[i]}`);
+      } else if(typeof objVals[i] === 'object'){
+        arrOfKeyVals.push('"'+ objKeys[i]+'":' + stringifyJSON(objVals[i]));
+      }
+    }
+  return '{' + arrOfKeyVals + '}';  
+  }
 };
+
